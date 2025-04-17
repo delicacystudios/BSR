8032fd4ab18901fd05c7d4c032f27de364c30b09
Delicacy Studios Inc. <95396564+delicacystudios@users.noreply.github.com>
2025-04-17 03:22:27 -0600

    v1.0.0

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
     bot: {
-        token: `MTMzNTkzMDE0ODY4MzUxMzkzNg.GV0_Vm.TsfGmLa9A9YMxsZoa8qivVSM7tKDILdN9oqDL0`,
+        token: ``,
         prefix: `b/`,
-        clientID: `1335930148683513936`,
-        db: `mongodb+srv://nateales:sdi21355@ulodat.lzajbec.mongodb.net/BSR?retryWrites=true&w=majority`
+        clientID: ``,
+        db: ``
     },
     
-    serverID: `1333769636340301835`,
+    serverID: ``,
 
     message: {
         colors: {
@@ -19,13 +19,13 @@ module.exports = {
     },
 
     roles: {
-        Leader: `1337001987530821744`,
-        Caller: `1335608428491440223`,
-        Combat: `1335607716827365497`,
-        Farmila: `1335608824614354995`,
-        Farmer: `1335609047998795847`,
-        Electro: `1335609609674686556`,
-        Builder: `1336282651966963724`,
-        member: `1335609830760779907`
+        Leader: ``,
+        Caller: ``,
+        Combat: ``,
+        Farmila: ``,
+        Farmer: ``,
+        Electro: ``,
+        Builder: ``,
+        member: ``
     }
 }
